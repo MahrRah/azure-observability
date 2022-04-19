7a9fff0d99fd3295b4312d8442b752e44dd2211c
Magdalena Baran <mbaran@microsoft.com>
2022-04-19 19:23:50 +0200

    Add basic RecipeAggregator Api with in memory database context

---
+++ web-app-workshop/RecipeAggregatorApi/Controllers/RecipesController.cs
@@ -0,0 +1,96 @@
+﻿#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeAggregatorApi.Models;
+
+namespace RecipeAggregatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly RecipeContext _context;
+
+        public RecipesController(RecipeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipes()
+        {
+            return await _context.Recipes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(long id)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return recipe;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipe(long id, Recipe recipe)
+        {
+            if (id != recipe.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRecipe), new { id = recipe.Id }, recipe);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipe(long id)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _context.Recipes.Remove(recipe);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RecipeExists(long id)
+        {
+            return _context.Recipes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ web-app-workshop/RecipeAggregatorApi/Models/Recipe.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeAggregatorApi.Models
+{
+    public class Recipe
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+    }
+}
